19458257ea198c418e346af67f9080e37d021892
Alex Wiltschko <alex.bw@gmail.com>
2014-08-22 09:36:38 -0400

    Fixing the multi-test sample problems

--- util.py
+++ util.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def sh(i, figsize=(8,1)):
+    from pylab import figure, imshow
+    figure(figsize=figsize)
+    if i.ndim == 3:
+        imshow(np.swapaxes(i,0,1))
+    else:
+        imshow(i.T)
+
 def get_norep(labels):
     labels_norep = []
     durations = []
@@ -56,3 +64,22 @@ def local_maxima(a):
 def get_boundary_data(data, k=7, sigma=3):
     w = np.r_[[0]*(k/2),data[k:] - data[:-k],[0]*(k/2)]
     return smooth(w,sigma)
+
+def extract_redness(img, saturation_threshold=0.5):
+    import skimage.color
+    img_lab = skimage.color.rgb2hsv(img).astype('float32')
+    saturation_img = img_lab[:,:,1]
+    value_img = img_lab[:,:,2]
+    value_img[saturation_img < saturation_threshold] = 1.0
+    SoverV = saturation_img/value_img
+    return SoverV
+    
+def detect_plug(this_data, threshold = 0.1, valid_range=(3,25)):
+    d = np.diff(this_data)
+    d[d<threshold] = 0.0
+    peaks = np.argwhere(local_maxima(d)).ravel()
+    peaks = peaks[(peaks > valid_range[0]) & (peaks < valid_range[1])]
+    if len(peaks) > 0:
+        return peaks[np.argmax(d[peaks]).ravel()[0]].ravel()[0]-1
+    else:
+        return -1
