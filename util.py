23836d435f54c65342b01502fc3a65d1c035bc89
Alex Wiltschko <alex.bw@gmail.com>
2015-01-06 17:23:22 -0500

    Explaining the preprocessing steps, and updating the extraction notebook

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.ndimage as ndimage
+import skimage.io
 
 def sh(i, figsize=(8,1)):
     from pylab import figure, imshow
@@ -20,6 +22,57 @@ def get_norep(labels):
             labels_norep.append(labels[i])
             durations.append(1)
     return np.array(labels_norep), np.array(durations)
+
+def process(img,downsample_fact=0.05):
+
+    # Downsample the image
+    r = []
+    for i in range(3):
+        r.append(ndimage.zoom(img[:,:,i], downsample_fact))
+    r = np.dstack(r)
+    rs = r.mean(2)
+
+    # Extract light/dark boundaries
+    h = ndimage.median_filter(np.max(rs,axis=1), (rs.shape[1]/10.0,))
+    w = ndimage.median_filter(np.max(rs,axis=0), (rs.shape[0]/50.0,))
+
+    # Find the index of the boundaries
+    height_ticks = find_highs(h,min_length=100)
+    width_ticks = find_highs(w,min_length=10)
+
+    # Extract the sub-images
+    test_imgs_bw = []
+    test_imgs = []
+    position = []
+    for iheight,height_tick in enumerate(height_ticks):
+        h1,h2 = height_tick
+        for iwidth,width_tick in enumerate(width_ticks):
+            w1,w2 = width_tick
+            Ibw = rs[h1:h2,w1:w2]
+            test_imgs_bw.append(Ibw)
+            I = r[h1:h2,w1:w2,:]
+            test_imgs.append(I)
+            position.append((iheight,iwidth))
+            
+    # Save a copy of the extracted images
+    out_imgs = test_imgs[:]
+            
+    # Shift the sub-images to remove the plug. 
+    shifts = []
+    for test_img, pos in zip(test_imgs, position):
+        plug_data = np.mean(test_img[:,:,0] - test_img.mean(2), axis=1)
+        plug_data = plug_data[2:] - plug_data[:-2] # lagged derivative, smooths things out a little
+        locs = np.argwhere(local_maxima(plug_data[:20])).ravel() # only look at the beginning of the tube for the plug
+        highs = [h+1 for h in locs[np.argsort(plug_data[locs])[::-1]]]
+        if len(highs) == 0:
+            print "NO PLUG FOUND FOR POSITION %s" % str(pos)
+        shifts.append(highs[0])
+    test_imgs = [I[shift:,:,:] for I,shift in zip(test_imgs, shifts)]
+    
+    # Extract fittable data
+    data = [-np.median(test_img,axis=1).mean(1) for test_img in test_imgs]
+
+    return data, position, out_imgs, shifts
 
 def find_highs(data, thresh=100, min_length=15):
     highs, durations = get_norep((data>thresh).astype('int32'))
